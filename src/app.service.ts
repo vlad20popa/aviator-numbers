8c2eff17555367b087906d27aa5db5d0d07cbfbd
Vlad Popa <vlad20popa@gmail.com>
2023-07-28 19:33:35 +0300

    add selenium and get number

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Builder, By } from 'selenium-webdriver';
+import * as fs from 'fs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello() {
+    const driver = await new Builder().forBrowser('chrome').build();
+
+    //Navigate to the url passed in
+    await driver.get('https://google.com');
+
+    const elements = await driver.findElement(By.id('L2AGLb')).click();
+    await driver.findElement(By.className('gLFyf')).sendKeys('test');
+    console.log(elements);
+
+    //Capture the screenshot
+    const image = await driver.takeScreenshot();
+
+    await fs.writeFileSync('./images/nyt-selenium.png', image, 'base64');
+    // await driver.quit();
+    return Promise.resolve();
   }
 }
