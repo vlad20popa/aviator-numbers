72e7d26de0be09991a2c01a71c207724b0bd8fa8
Vlad Popa <vlad20popa@gmail.com>
2023-08-02 12:18:00 +0300

    use node finder

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,18 +1,38 @@
-import { Injectable } from '@nestjs/common';
-import { Builder, By } from 'selenium-webdriver';
-import * as fs from 'fs';
+import { Injectable } from "@nestjs/common";
+import { Builder, By } from "selenium-webdriver";
+import * as fs from "fs";
 
 @Injectable()
 export class AppService {
+
+  sleep = (ms) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  };
+
   async getHello() {
     const driver = await new Builder().forBrowser('chrome').build();
 
     //Navigate to the url passed in
-    await driver.get('https://google.com');
+    await driver.get(
+      'https://www.maxbet.ro/ro/jocuri-pacanele-online/aviator-spribe',
+    );
 
-    const elements = await driver.findElement(By.id('L2AGLb')).click();
-    await driver.findElement(By.className('gLFyf')).sendKeys('test');
-    console.log(elements);
+    while (true) {
+      try {
+        const button = await driver.findElement(
+          // By.xpath("//button[@class='btn.btn-success.bet.ng-star-inserted']"),
+          (By.css("div[class='btn btn-success bet ng-star-inserted']")),
+          // By.className('btn.btn-success.bet.ng-star-inserted'),
+        );
+        console.log(button.isEnabled());
+        await this.sleep(500);
+      } catch (e) {
+        console.log('no element found');
+        console.log(e.message);
+      }
+    }
 
     //Capture the screenshot
     const image = await driver.takeScreenshot();
